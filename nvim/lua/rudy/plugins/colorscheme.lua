26114084c02a17007d66fa01f8039559ad4f65fc
Rudy <rudyhartwig25@outlook.com>
2024-06-19 16:53:44 -0400

    chore(colorscheme): removed loading that is now done in init.lua

--- nvim/lua/rudy/plugins/colorscheme.lua
+++ nvim/lua/rudy/plugins/colorscheme.lua
@@ -5,8 +5,5 @@ return {
   config = function()
     -- load catppuccin and set visual modifications
     vim.cmd([[colorscheme catppuccin-mocha]])
-    vim.cmd([[set number]])
-    vim.cmd([[set nowrap]])
-    vim.cmd([[set termguicolors]])
   end,
 }
